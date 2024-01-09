87f48ede044e0b2105455a290e9edbf799e7b38d
Xmithie <Xmithie@hacking.com>
2024-01-09 00:08:41 -0500

    Aisk

--- src/public/js/script.js
+++ src/public/js/script.js
@@ -1,4 +1,4 @@
 function random() {
     const aisk = ["Aisk", "🍩", "🍭", "🍡", "Snokmode", "Keith"]
     document.getElementById('randy').innerHTML = aisk[Math.floor(Math.random() * aisk.length)]
-}
+}n
